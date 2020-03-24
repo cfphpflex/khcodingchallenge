b86dbdd69a67100d9353424f63b14cc31ccf9701
Emiliano Gaytan <cfphpflex@gmail.com>
2020-03-24 06:05:30 -0700

    reduce prohibitive large amount of files for code review

--- resources/views/part_scripts.php
+++ resources/views/part_scripts.php
@@ -1,9 +1,5 @@
-    <!-- Mainly scripts -->
 
-    <script src="js/plugins/metisMenu/jquery.metisMenu.js"></script>
-    <script src="js/plugins/slimscroll/jquery.slimscroll.min.js"></script>
-
-    <!--  Datatables -->
+<!--  Datatables -->
     <script src="js/plugins/jeditable/jquery.jeditable.js"></script>
     <script src="js/plugins/dataTables/jquery.dataTables.min.js"></script>
      <script src="js/plugins/dataTables/jquery.dataTables.editable.js"></script>
@@ -11,7 +7,6 @@
     <script src="js/plugins/dataTables/dataTables-1.5.6.buttons.min.js"></script>
     <script src="js/plugins/dataTables/buttons.flash.min.js"></script>
     <script src="js/plugins/dataTables/jszip.min.js"></script>
-<!--    <script src="js/plugins/dataTables/pdfmake.min.js"></script>-->
     <script src="js/plugins/dataTables/vfs_fonts.js"></script>
     <script src="js/plugins/dataTables/buttons.html5.min.js"></script>
     <script src="js/plugins/dataTables/buttons.print.min.js"></script>
